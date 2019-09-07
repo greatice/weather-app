a1613221fedf5a6b85700a36df4160dd83cc64d3
pei luo <alexluolibra@hotmail.com>
2019-09-07 10:46:41 +1000

    first stage

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Store extends React.Component {
+    constructor(props){
+        super(props);
+        //This is the main App state
+        this.state = {
+            appName: "Weather-App"
+        };
+    }
+
+    render () {
+        return React.Children.map(this.props.children, (child)=> {
+            return React.cloneElement(child,{ ...this.state});
+        });
+    }
+}
+
+//Try to traverse every child props
